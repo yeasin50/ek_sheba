6118d0eb4ff01dc294c199c3b2b061859e82aeaf
yeasin50 <yeasinsheikh50@gmail.com>
2023-07-04 19:18:38 +0600

    inital-loaded

--- lib/src/features/helpDesk/presentation/pages/guidline_page.dart
+++ lib/src/features/helpDesk/presentation/pages/guidline_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:collection/collection.dart';
 import 'package:ek_sheba/src/locator.dart';
 import 'package:flutter/material.dart';
@@ -27,6 +29,11 @@ class GuideLinePage extends StatelessWidget {
                 } else if (state is GuidelineError) {
                   return Center(child: Text(state.message));
                 } else if (state is GuidelineLoaded) {
+                  scheduleMicrotask(() {
+                    if (state.guidelines.isNotEmpty) {
+                      locator.get<GuidelineBloc>().add(GuidelineSelectEvent(imsModuleId: state.guidelines.first.uuid));
+                    }
+                  });
                   return _OnGuidelineLoadView(loadedState: state);
                 } else {
                   return Text("NA State $state");
