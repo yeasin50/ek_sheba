f0df500acf3dbae3cc001245aba5739b61e687de
yeasin50 <yeasinsheikh50@gmail.com>
2023-07-05 22:48:44 +0600

    rm_temp

--- lib/src/features/helpDesk/data/datasources/notice_plan_temp_db.dart
+++ lib/src/features/helpDesk/data/datasources/notice_plan_temp_db.dart
@@ -1,31 +1,31 @@
 import 'package:ek_sheba/src/features/helpDesk/domain/entities/notice_info.dart';
 
 const noticePlanTempDB = [
-  NoticeInfo(
-    title: 'Monthly Co-ordination Meeting Notice',
-    description:
-        'Published Date: 15-September, 2022 Summary: Title, 8th Five Year Plan, July 2021 - June 2025 : promoting prosperity and fostering inclusiveness. Publication Type, Book. Year of Publication, 2020.',
-    url: 'https://www.google.com',
-  ),
-  NoticeInfo(
-    title: 'List of Participant PPS EUT 7th Batch',
-    description: 'List of Participant PPS EUT 7th Batch',
-    url: 'https://www.google.com',
-  ),
-  NoticeInfo(
-    title: 'Planning Division Welfare Association...',
-    description: 'Summary: Title, 7th Five Year Plan, July 2016 - June 2021 : promoting prosperity and fostering inclusiveness. Publication Type, Book. Year of Publication, 2016.',
-    url: 'https://www.google.com',
-  ),
-  NoticeInfo(
-    title: 'Public Opinion Meeting Notice',
-    description:
-        'Published Date: 13-September, 2022 Summary: Title, 6th Five Year Plan, July 2011 - June 2016 : promoting prosperity and fostering inclusiveness. Publication Type, Book. Year of Publication, 2011.',
-    url: 'https://www.google.com',
-  ),
-  NoticeInfo(
-    title: 'Yearly Purchase Plan Notice',
-    description: 'Yearly Purchase Plan Notice: promoting prosperity and fostering inclusiveness. Publication Type, Book. Year of Publication, 2011.',
-    url: 'https://www.google.com',
-  ),
+  // NoticeInfo(
+  //   title: 'Monthly Co-ordination Meeting Notice',
+  //   description:
+  //       'Published Date: 15-September, 2022 Summary: Title, 8th Five Year Plan, July 2021 - June 2025 : promoting prosperity and fostering inclusiveness. Publication Type, Book. Year of Publication, 2020.',
+  //   url: 'https://www.google.com',
+  // ),
+  // NoticeInfo(
+  //   title: 'List of Participant PPS EUT 7th Batch',
+  //   description: 'List of Participant PPS EUT 7th Batch',
+  //   url: 'https://www.google.com',
+  // ),
+  // NoticeInfo(
+  //   title: 'Planning Division Welfare Association...',
+  //   description: 'Summary: Title, 7th Five Year Plan, July 2016 - June 2021 : promoting prosperity and fostering inclusiveness. Publication Type, Book. Year of Publication, 2016.',
+  //   url: 'https://www.google.com',
+  // ),
+  // NoticeInfo(
+  //   title: 'Public Opinion Meeting Notice',
+  //   description:
+  //       'Published Date: 13-September, 2022 Summary: Title, 6th Five Year Plan, July 2011 - June 2016 : promoting prosperity and fostering inclusiveness. Publication Type, Book. Year of Publication, 2011.',
+  //   url: 'https://www.google.com',
+  // ),
+  // NoticeInfo(
+  //   title: 'Yearly Purchase Plan Notice',
+  //   description: 'Yearly Purchase Plan Notice: promoting prosperity and fostering inclusiveness. Publication Type, Book. Year of Publication, 2011.',
+  //   url: 'https://www.google.com',
+  // ),
 ];
