265d3f01e6ce8bd71cf375e2ab328cd4eec2e4b7
yeasin50 <yeasinsheikh50@gmail.com>
2023-09-26 13:17:53 +0600

    added-header

--- lib/src/features/IDSDP/presentation/widgets/dashboard_listview.dart
+++ lib/src/features/IDSDP/presentation/widgets/dashboard_listview.dart
@@ -15,51 +15,7 @@ class DashboardListView extends StatelessWidget {
   final String itemTitle;
   @override
   Widget build(BuildContext context) {
-    const slWidth = 30.0;
-    const totalCostWidth = 95.0;
-
-    final headerTextStyles = GoogleFonts.poppins(
-      color: AppStyle.textWhite,
-      fontSize: 14,
-      fontWeight: FontWeight.w500,
-    );
-    final header = Material(
-      elevation: 2,
-      color: AppStyle.buttonGreen,
-      borderRadius: BorderRadius.circular(4),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(
-              width: slWidth,
-              child: Text("SL", style: headerTextStyles),
-            ),
-            Expanded(
-              child: Text(
-                "Project Information",
-                style: headerTextStyles,
-              ),
-            ),
-            SizedBox(
-              width: totalCostWidth,
-              child: Text(
-                "Project Cost",
-                style: headerTextStyles,
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-    return Column(
-      children: [
-        header,
-        getItem(itemTitle),
-      ],
-    );
+    return getItem(itemTitle);
   }
 }
 
--- lib/src/features/IDSDP/presentation/widgets/project_list_widget_future.dart
+++ lib/src/features/IDSDP/presentation/widgets/project_list_widget_future.dart
@@ -1,7 +1,10 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_utils/my_utils.dart';
 
+import '../../../../common/app_style.dart';
 import '../../../../common/utils/logger.dart';
 import '../../domain/entities/project_details.dart';
 import '../pages/project_details_page.dart';
@@ -33,26 +36,77 @@ class ProjectListFromFuture extends StatelessWidget {
           logger.d('getItem: ${snapshot.data}');
 
           return Column(
-            children: snapshot.data != null
-                ? snapshot.data!
-                    .mapIndexed(
-                      (sl, e) => ProjectPlanInfoCard(
-                        sl: sl + 1,
-                        projectDetails: e,
-                        onTap: () {
-                          if (context.mounted) {
-                            context.push(ProjectDetailsPage.routeName, extra: e);
-                          }
-                        },
-                      ),
-                    )
-                    .toList()
-                : [],
+            children: [
+              const _BuildHeader(),
+              const SizedBox(height: 8),
+              ...snapshot.data != null
+                  ? snapshot.data!
+                      .mapIndexed(
+                        (sl, e) => ProjectPlanInfoCard(
+                          sl: sl + 1,
+                          projectDetails: e,
+                          onTap: () {
+                            if (context.mounted) {
+                              context.push(ProjectDetailsPage.routeName, extra: e);
+                            }
+                          },
+                        ),
+                      )
+                      .toList()
+                  : [],
+            ],
           );
         }
 
         return const Center(child: Text('NA state'));
       },
+    );
+  }
+}
+
+class _BuildHeader extends StatelessWidget {
+  const _BuildHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const slWidth = 30.0;
+    const totalCostWidth = 95.0;
+
+    final headerTextStyles = GoogleFonts.poppins(
+      color: AppStyle.textWhite,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+    );
+    return Material(
+      elevation: 2,
+      color: AppStyle.buttonGreen,
+      borderRadius: BorderRadius.circular(4),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: slWidth,
+              child: Text("SL", style: headerTextStyles),
+            ),
+            Expanded(
+              child: Text(
+                "Project Information",
+                style: headerTextStyles,
+              ),
+            ),
+            SizedBox(
+              width: totalCostWidth,
+              child: Text(
+                "Project Cost",
+                style: headerTextStyles,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
