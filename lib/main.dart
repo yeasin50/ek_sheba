2f76836d805c9748a7977c3e7c5d779c90558597
yeasin50 <yeasinsheikh50@gmail.com>
2023-10-27 15:18:45 +0600

    upgrade-patch_pal

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:ek_sheba/src/common/utils/logger.dart';
 import 'package:ek_sheba/src/common/utils/token_storage.dart';
 import 'package:ek_sheba/src/locator.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:patch_pal/patch_pal.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 import 'src/ek_sheba_app.dart';
 
@@ -13,7 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
-  await PatchPal().setUp('https://raw.githubusercontent.com/yeasin50/AssetsFor_/master/apps/patch_pal/ek_sheba.json');
+  await PatchPal.init('https://raw.githubusercontent.com/yeasin50/AssetsFor_/master/apps/patch_pal/ek_sheba.json');
   await TokenManager.setUp();
   setup();
   runApp(
@@ -32,7 +29,7 @@ class _Patcher extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final item = PatchPal().getItem('patchedEnabled');
+    final item = PatchPal.getItem('patchedEnabled');
 
     if (item?.value == true) {
       return const EkSheba();
