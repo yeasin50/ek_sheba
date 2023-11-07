aa6f684a2ebaffd416e9f2c35d1b064083f56974
yeasin50 <yeasinsheikh50@gmail.com>
2023-11-07 17:01:35 +0600

    fixed-comment

--- lib/src/features/IDSDP/presentation/utils/comment_on_pdf_details.dart
+++ lib/src/features/IDSDP/presentation/utils/comment_on_pdf_details.dart
@@ -1,5 +1,6 @@
 import 'package:ek_sheba/src/common/utils/logger.dart';
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
 import '../../data/repositories/comment_repo_impl.dart';
 import '../../domain/entities/project_details.dart';
@@ -8,18 +9,24 @@ Future<void> comment({
   required BuildContext context,
   required ProjectDetails project,
 }) async {
+  final isBn = project.isForeignAid;
   await showDialog(
     context: context,
     builder: (context) {
       return AlertDialog(
         scrollable: false,
-        title: const Text('মন্তব্য/পর্যবেক্ষণ'),
+        title: Text(isBn ? 'মন্তব্য' : 'Comment'),
         content: Material(
           child: FutureBuilder(
             future: CommentRepoImpl.getData(project),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
+                return const Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Center(child: CircularProgressIndicator()),
+                  ],
+                );
               }
 
               if (snapshot.hasError) {
@@ -28,17 +35,30 @@ Future<void> comment({
 
               if (snapshot.hasData) {
                 logger.i("data ${snapshot.data}");
+                final data = snapshot.data ?? [];
+
+                final items = groupBy(data, (p0) => p0.observer);
                 return SingleChildScrollView(
                   child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      ...snapshot.data
-                              ?.map<Widget>(
-                                (e) => ListTile(
-                                  title: Text(e.comment),
-                                ),
+                      if (items.isEmpty) Center(child: Text(isBn ? 'কোন মন্তব্য নেই' : 'No comments found')),
+                      ...items.entries.map(
+                        (e) {
+                          return ExpansionTile(
+                            title: Text(e.key),
+                            children: [
+                              ...e.value.map(
+                                (e) {
+                                  return ListTile(
+                                    title: Text(e.comment),
+                                  );
+                                },
                               )
-                              .toList() ??
-                          [],
+                            ],
+                          );
+                        },
+                      )
                     ],
                   ),
                 );
